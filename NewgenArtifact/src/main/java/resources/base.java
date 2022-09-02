5c824303f28a2e377e634c5c3c9a75f5a822e37b
nitik <nitikaaggarwal2011@yahoo.com>
2022-09-02 18:37:34 +0530

    committed requested

--- NewgenArtifact/src/main/java/resources/base.java
+++ NewgenArtifact/src/main/java/resources/base.java
@@ -2,35 +2,46 @@ package resources;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-
+import java.time.Duration;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
 	
-	public WebDriver driver;
-	public Properties prop;
+	public static WebDriver driver;
+	public static Properties prop;
 	
+	public base() throws IOException {
+		prop=new Properties();
+		FileInputStream fis=new FileInputStream("C:\\Users\\nitik\\git\\repository2\\NewgenArtifact\\src\\main\\java\\resources\\data.properties");
+		prop.load(fis);	
+	}
 	
-    public String getEdgeBrowser() throws IOException {
-		prop=new Properties();
-		FileInputStream fis=new FileInputStream("D:\\Selenium\\SeleniumWorkspace\\NewgenArtifact\\src\\main\\java\\resources\\data.properties");
-		prop.load(fis);
-		String browserName=prop.getProperty("browser");
-		System.out.println("broswer invoked successfully"+ browserName);
-		
-		return browserName;
-	}
-    
-    public String getChromeBrowser() throws IOException {
-		prop=new Properties();
-		FileInputStream fis=new FileInputStream("D:\\Selenium\\SeleniumWorkspace\\NewgenArtifact\\src\\main\\java\\resources\\data.properties");
-		prop.load(fis);
-		String browserName=prop.getProperty("Chrome");
-		System.out.println("broswer invoked successfully"+ browserName);
-		
-		return browserName;
-	}
-    
+    public static void openBrowser(String browser) {
+    	if(browser.equalsIgnoreCase("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+		   driver= new ChromeDriver();
+			
+		}
+		else if(browser.equalsIgnoreCase("Firefox"))
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("Edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver= new EdgeDriver();
+		}
+    	
+    	driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 }
--- NewgenArtifact/src/test/java/Nitika/newGenAssignment.java
+++ NewgenArtifact/src/test/java/Nitika/newGenAssignment.java
@@ -1,21 +1,15 @@
 package Nitika;
+
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import pageObjects.fogotPaswd;
 import pageObjects.pageObjects;
 import pageObjects.resetPaswd;
@@ -23,123 +17,233 @@ import pageObjects.yopmailVerify;
 import resources.base;
 
 @Test
-public class newGenAssignment extends base{
-	
-	    public WebDriver driver;
-	
-	    
-	  //  @Parameters("browser")
-	   // @BeforeTest
-		public void initialize() throws IOException
-		{
-	    	// String broswerName=browser;
-	    	String broswerName=getEdgeBrowser();
-	    	System.out.println(broswerName);
-	    		
-			if(broswerName.equalsIgnoreCase("Chrome"))
-			{
-				System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\ChromeDriver\\chromedriver.exe");
-				
-				ChromeOptions options=new ChromeOptions();
-				if(broswerName.contains("headless"))
-				{ options.addArguments("headless"); }
-				driver= new ChromeDriver(options);
-				
-			}
-			else if(broswerName.equalsIgnoreCase("Firefox"))
-			{
-				
-			}
-			else if(broswerName.equalsIgnoreCase("Edge"))
-			{
-				System.setProperty("webdriver.edge.driver", "D:\\Selenium\\EdgeDriver\\msedgedriver.exe");
+public class newGenAssignment extends base {
 
-				driver= new EdgeDriver();
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	public newGenAssignment() throws IOException {
+		super();
+	}
+
+	@BeforeTest
+	public void getStarted() {
+		System.out.println("execution started");
+	}
+
+	@Test
+	public void loginVerifyOnChrome() throws InterruptedException {
+
+		System.out.println("Chrome browser");
+		openBrowser("Chrome");
+		driver.get(prop.getProperty("url"));
+
+		// 1. login credentials are valid or not
+		// 2. check forgotten Password
+		pageObjects obj = new pageObjects(driver);
+		obj.myacc().click();
+		obj.signin().click();
+		String testEmail = prop.getProperty("testEmail");
+		obj.getemail().sendKeys(testEmail);
+
+		String Paswd = prop.getProperty("wrongPaswd");
+		obj.getpaswd().sendKeys(Paswd);
+		obj.btnclk().click();
+
+		int size = driver.findElements(By.xpath("//div[@class='help-block']")).size();
+
+		if (size == 1) {
+			fogotPaswd fp = obj.forgotpd();
+			fp.getemailAddress().sendKeys(testEmail);
+
+			yopmailVerify yopObj = fp.sendBtn();
+			driver.get(prop.getProperty("yopmailUrl"));
+			yopObj.getemailAddress().sendKeys(testEmail);
+			yopObj.search().click();
+			yopObj.frameLeft();
+			yopObj.paswdTextLeft().click();
+			yopObj.frameEmail();
+
+			String win1 = driver.getWindowHandle();
+			resetPaswd rp = yopObj.forgotLink();
+			Set<String> wins = driver.getWindowHandles();
+			for (String w : wins) {
+				if (w != win1)
+					driver.switchTo().window(w);
 			}
-			
-		}
-	    
-	    @Test(priority=0)
-	    public void loginVerify() {
-	    	driver.get(prop.getProperty("url"));
-	  
-	    	pageObjects obj=new pageObjects(driver);
-	    	obj.myacc().click();
-	    	obj.signin().click();
-	    	String testEmail=prop.getProperty("testEmail");
-	    	obj.getemail().sendKeys(testEmail);
-	    	
-	    	String Paswd=prop.getProperty("correctPaswd");
-	    	obj.getpaswd().sendKeys(Paswd);
-	    	obj.btnclk().click();
-	   
-	    	int size= driver.findElements(By.xpath("//div[@class='help-block']")).size();
-	    	
-	    	if(size==1) 
-	    	{
-	    	fogotPaswd fp=obj.forgotpd();
-	    	fp.getemailAddress().sendKeys(testEmail);
-	    	
-	    	yopmailVerify y=fp.sendBtn();
-	    	driver.get(prop.getProperty("yopmailUrl"));
-	    	y.getemailAddress().sendKeys(testEmail); 
-	    	y.search().click();
-	    	y.frameLeft();
-	    	y.paswdTextLeft().click();
-	    	y.frameEmail();
-	    	
-	    	String win1=driver.getWindowHandle();
-	    	resetPaswd rp=    y.forgotLink();
-	    	Set<String> wins=driver.getWindowHandles();
-	    	for(String w: wins) {
-	    		if(w!=win1)
-	    			driver.switchTo().window(w);
-	    	}
-	    
-	    	rp.paswdEnter().sendKeys("meenu@1986");
-	    	rp.confirmPaswd().sendKeys("meenu@1986");
-	    rp.changePasBtn().click();
-	    	}	
-	    	
-	    	else {
-	    		driver.get(prop.getProperty("loginURL"));
-	    	}
-			
-		}
-	    
-	    @Test(priority=2)
-	    public void setBroswerSize() {
-	    	
-	    	Dimension d1=driver.manage().window().getSize();
-	        int h=d1.getHeight();
-	        int w=d1.getWidth();
-	        System.out.println(h);
-	        System.out.println(w);
-	        
-	    	Dimension d=new Dimension(w-88,h-67);
-	    	driver.manage().window().setSize(d);	
-	    }
-	    
-	    @Test(priority=1)
-	    public void clkOffset() throws InterruptedException {
-	    	
-	    	Thread.sleep(5000);
-	    	WebElement ele=driver.findElement(By.xpath("//span[contains(text(),'Need help')]"));
-	    	
-	    
-            int xC=ele.getLocation().getX();
-	        int yC=ele.getLocation().getY();
-	        Actions act=new Actions(driver);
-	        act.moveByOffset(xC, yC).click().build().perform();
-	    }
-	    
-	    @AfterTest 
-		public void teardown()
-		{
-			driver.close();
+
+			rp.paswdEnter().sendKeys("meenu@1986");
+			rp.confirmPaswd().sendKeys("meenu@1986");
+			rp.changePasBtn().click();
 		}
 
+		else {
+			driver.get(prop.getProperty("loginURL"));
+		}
+
+		// 4.click some special coordinate set by user(take some user input from config
+		// file)
+		Thread.sleep(5000);
+		WebElement ele = driver.findElement(By.xpath("//span[contains(text(),'Need help')]"));
+
+		int xC = ele.getLocation().getX();
+		int yC = ele.getLocation().getY();
+		Actions act = new Actions(driver);
+		act.moveByOffset(xC, yC).click().build().perform();
+
+		// 3.set resolution (set dimension value based on that web page open as full
+		// screen)
+		Dimension d1 = driver.manage().window().getSize();
+		int h = d1.getHeight();
+		int w = d1.getWidth();
+
+		Dimension d = new Dimension(w - 88, h - 67);
+		driver.manage().window().setSize(d);
+		driver.close();
+
+	}
+
+	@Test
+	public void loginVerifyOnEdge() throws InterruptedException {
+		System.out.println("Edge browser");
+		openBrowser("Edge");
+		driver.get(prop.getProperty("url"));
+
+		// 1. login credentials are valid or not
+		// 2. check forgotten Password
+		pageObjects obj = new pageObjects(driver);
+		obj.myacc().click();
+		obj.signin().click();
+		String testEmail = prop.getProperty("testEmail");
+		obj.getemail().sendKeys(testEmail);
+
+		String Paswd = prop.getProperty("correctPaswd");
+		obj.getpaswd().sendKeys(Paswd);
+		obj.btnclk().click();
+
+		int size = driver.findElements(By.xpath("//div[@class='help-block']")).size();
+
+		if (size == 1) {
+			fogotPaswd fp = obj.forgotpd();
+			fp.getemailAddress().sendKeys(testEmail);
+
+			yopmailVerify y = fp.sendBtn();
+			driver.get(prop.getProperty("yopmailUrl"));
+			y.getemailAddress().sendKeys(testEmail);
+			y.search().click();
+			y.frameLeft();
+			y.paswdTextLeft().click();
+			y.frameEmail();
+
+			String win1 = driver.getWindowHandle();
+			resetPaswd rp = y.forgotLink();
+			Set<String> wins = driver.getWindowHandles();
+			for (String w : wins) {
+				if (w != win1)
+					driver.switchTo().window(w);
+			}
+
+			rp.paswdEnter().sendKeys("meenu@1986");
+			rp.confirmPaswd().sendKeys("meenu@1986");
+			rp.changePasBtn().click();
+		}
+
+		else {
+			driver.get(prop.getProperty("loginURL"));
+		}
+
+		// 4.click some special coordinate set by user(take some user input from config
+		// file)
+		Thread.sleep(5000);
+		WebElement ele = driver.findElement(By.xpath("//span[contains(text(),'Need help')]"));
+
+		int xC = ele.getLocation().getX();
+		int yC = ele.getLocation().getY();
+		Actions act = new Actions(driver);
+		act.moveByOffset(xC, yC).click().build().perform();
+
+		// 3.set resolution (set dimension value based on that web page open as full
+		// screen)
+		Dimension d1 = driver.manage().window().getSize();
+		int h = d1.getHeight();
+		int w = d1.getWidth();
+
+		Dimension d = new Dimension(w - 88, h - 67);
+		driver.manage().window().setSize(d);
+		driver.close();
+	}
+
+	@Test
+	public void loginVerifyOnFirefox() throws InterruptedException {
+		System.out.println("Firefox browser");
+		openBrowser("Firefox");
+		driver.get(prop.getProperty("url"));
+
+		// 1. login credentials are valid or not
+		// 2. check forgotten Password
+		pageObjects obj = new pageObjects(driver);
+		obj.myacc().click();
+		obj.signin().click();
+		String testEmail = prop.getProperty("testEmail");
+		obj.getemail().sendKeys(testEmail);
+
+		String Paswd = prop.getProperty("correctPaswd");
+		obj.getpaswd().sendKeys(Paswd);
+		obj.btnclk().click();
+
+		int size = driver.findElements(By.xpath("//div[@class='help-block']")).size();
+
+		if (size == 1) {
+			fogotPaswd fp = obj.forgotpd();
+			fp.getemailAddress().sendKeys(testEmail);
+
+			yopmailVerify y = fp.sendBtn();
+			driver.get(prop.getProperty("yopmailUrl"));
+			y.getemailAddress().sendKeys(testEmail);
+			y.search().click();
+			y.frameLeft();
+			y.paswdTextLeft().click();
+			y.frameEmail();
+
+			String win1 = driver.getWindowHandle();
+			resetPaswd rp = y.forgotLink();
+			Set<String> wins = driver.getWindowHandles();
+			for (String w : wins) {
+				if (w != win1)
+					driver.switchTo().window(w);
+			}
+
+			rp.paswdEnter().sendKeys("meenu@1986");
+			rp.confirmPaswd().sendKeys("meenu@1986");
+			rp.changePasBtn().click();
+		}
+
+		else {
+			driver.get(prop.getProperty("loginURL"));
+		}
+
+		// 4.click some special coordinate set by user(take some user input from config
+		// file)
+		Thread.sleep(5000);
+		WebElement ele = driver.findElement(By.xpath("//span[contains(text(),'Need help')]"));
+
+		int xC = ele.getLocation().getX();
+		int yC = ele.getLocation().getY();
+		Actions act = new Actions(driver);
+		act.moveByOffset(xC, yC).click().build().perform();
+
+		// 3.set resolution (set dimension value based on that web page open as full
+		// screen)
+		Dimension d1 = driver.manage().window().getSize();
+		int h = d1.getHeight();
+		int w = d1.getWidth();
+
+		Dimension d = new Dimension(w - 88, h - 67);
+		driver.manage().window().setSize(d);
+
+	}
+
+	@AfterTest
+	public void teardown() {
+		driver.quit();
+		System.out.println("execution finished");
+	}
 
 }
